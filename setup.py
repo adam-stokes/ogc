6039d42ba0de0248de6b632b4c323a5b062108df
Adam Stokes <battlemidget@users.noreply.github.com>
2019-07-27 14:38:17 -0400

    Version updated from 0.2.8 to 0.3.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = README.read_text(encoding="utf8")
 
 setuptools.setup(
     name="ogc",
-    version="0.2.8",
+    version="0.3.0",
     author="Adam Stokes",
     author_email="adam.stokes@ubuntu.com",
     description="ogc, a runner of things",
