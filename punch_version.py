8289062ff94a0b8c987379eef7b28482e9a22922
Adam Stokes <battlemidget@users.noreply.github.com>
2019-08-08 11:59:29 -0400

    Version updated from 0.3.12 to 0.3.13

--- punch_version.py
+++ punch_version.py
@@ -1,3 +1,3 @@
 major = 0
 minor = 3
-patch = 12
+patch = 13
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = README.read_text(encoding="utf8")
 
 setuptools.setup(
     name="ogc",
-    version="0.3.12",
+    version="0.3.13",
     author="Adam Stokes",
     author_email="adam.stokes@ubuntu.com",
     description="ogc, a runner of things",
