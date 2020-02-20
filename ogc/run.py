beb0eef35b3733104c0109913d95c56ea077d714
Adam Stokes <battlemidget@users.noreply.github.com>
2020-02-20 14:45:09 -0500

    revert output to stdout
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- ogc/run.py
+++ ogc/run.py
@@ -19,7 +19,7 @@ def _log_sub_out(pipe):
     """ Logs output from subprocess
     """
     for line in iter(pipe.readline, b""):
-        app.log.debug(line.decode().strip())
+        app.log.info(line.decode().strip())
 
 
 def script(script_data, env, **kwargs):
