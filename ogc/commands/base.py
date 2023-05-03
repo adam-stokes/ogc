6591284648ac9706d2c6baa086b81cf3a8b6e83a
Adam Stokes <51892+adam-stokes@users.noreply.github.com>
2023-05-03 11:01:31 -0400

    feat: add layout subcommand
    Add ability to import layouts from a yml specification. The groundwork
    will provide quick access to a single layout, set of layouts, queried
    layouts by tag and allow for applying tests/scripts to a wide
    range of variations quickly.

--- ogc/commands/base.py
+++ ogc/commands/base.py
@@ -1,64 +1,23 @@
 from __future__ import annotations
 
 import os
-import sys
-from concurrent.futures import ThreadPoolExecutor, wait
-from functools import partial
 from multiprocessing import cpu_count
-from pathlib import Path
 
 import click
 from dotenv import load_dotenv
 
-import ogc.loader
-import ogc.signals
-from ogc import db
 from ogc.log import get_logger
-from ogc.models import actions, layout, machine, tags
 
 # Not advertised, but available for those who seek moar power.
 MAX_WORKERS = int(os.environ.get("OGC_MAX_WORKERS", cpu_count() - 1))
 
-dbi = db.connect()
-dbi.create_tables(
-    [machine.MachineModel, tags.TagModel, layout.LayoutModel, actions.ActionModel]
-)
 
-
-def run_spec(spec: Path, task: str, **opts) -> None:
-    log = get_logger("ogc")
-    mod = ogc.loader.from_path(spec)
-    if not mod:
-        log.error(f"Could not load {spec} into OGC.")
-        sys.exit(1)
-    ogc.loader.run(mod, task, **opts)
-
-
-@click.command(help="Manage and Provision machines")
-@click.option(
-    "--options",
-    "-o",
-    multiple=True,
-    help="Pass in -o KEY=VALUE -o KEY=VALUE which is used in the provision spec",
-    metavar="KEY=VAL",
-)
-@click.option("--verbose", "-v", required=False, help="Turn on debug", is_flag=True)
-@click.argument("spec", type=Path)
-@click.argument("task", type=str)
-def cli(options: list[str], verbose: bool, spec: Path, task: str) -> None:
+@click.option("--verbose", "-v", is_flag=True, help="Increase logging verbosity")
+@click.group()
+def cli(verbose: bool) -> None:
     """Just a simple provisioner"""
     load_dotenv()
     get_logger("ogc", verbose)
-    opts = {opt.split("=")[0]: opt.split("=")[1] for opt in options}
-
-    has_multiple_specs = list(spec.glob("*.py"))
-    if has_multiple_specs:
-        with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-            func = partial(run_spec, task=task, opts=opts)
-            results = [executor.submit(func, spec) for spec in has_multiple_specs]
-            wait(results, timeout=5)
-    else:
-        run_spec(spec, task, **opts)
 
 
 def start() -> None:
---
+++ ogc/commands/layout.py
@@ -0,0 +1,44 @@
+"""Manages layouts"""
+from __future__ import annotations
+
+import sys
+from pathlib import Path
+
+import click
+import yaml
+
+from ogc import db
+from ogc.commands.base import cli
+from ogc.log import get_logger
+from ogc.models import actions, layout, machine, tags
+
+dbi = db.connect()
+dbi.create_tables(
+    [machine.MachineModel, tags.TagModel, layout.LayoutModel, actions.ActionModel]
+)
+
+
+@click.group()
+def _layout() -> None:
+    """Layout manager"""
+
+
+@click.command(help="Add/Modify/Delete layouts")
+@click.argument("spec", type=Path, metavar="<layouts.yml>")
+def _import(spec: Path) -> None:
+    """Manages layouts"""
+    log = get_logger("ogc.commands.layout.add")
+    if not spec.exists():
+        log.critical(f"Unable to locate {spec}, please double check the path.")
+        sys.exit(1)
+
+    layouts_from_spec = yaml.safe_load(spec.read_text())
+    for item in layouts_from_spec["layouts"]:
+        layout.LayoutModel.get_or_create(**item)
+
+    for item in layout.LayoutModel.select():
+        log.info(f"Added layout: {item.id}:{item.runs_on}")
+
+
+_layout.add_command(_import, name="import")
+cli.add_command(_layout, name="layout")
